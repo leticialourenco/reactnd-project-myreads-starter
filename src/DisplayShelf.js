224ebf7214e9f703befd3008d61f86acd334b52c
Leticia <leticia@Leticias-MacBook-Pro.local>
2018-02-24 23:45:32 -0800

    fix: Empty search results on empty query

--- src/DisplayShelf.js
+++ src/DisplayShelf.js
@@ -5,25 +5,34 @@ import escapeRegExp from 'escape-string-regexp'
 class DisplayShelf extends Component {
     render() {
         const { books } = this.props.books
-        let bookList = books.filter((book) => book.shelf === this.props.value)
+        let bookList = ''
 
         if (this.props.title === 'searchResults') {
-            let match = new RegExp(escapeRegExp(this.props.value), 'i')
-            bookList = books.filter((book) => (match.test(book.title)) || (match.test(book.authors)))
+            if (this.props.value !== '') {
 
-            return (
-                <div>
-                    <ol className="books-grid">
-                        { bookList.map((book) => (
-                            <li key={book.id}>
-                                <BookItem
-                                    book={book}
-                                />
-                            </li>
-                        ))}
-                    </ol>
-                </div>
-            )} else {
+                let match = new RegExp(escapeRegExp(this.props.value), 'i')
+                bookList = books.filter((book) => (match.test(book.title)) || (match.test(book.authors)))
+
+                return (
+                    <div>
+                        <ol className="books-grid">
+                            { bookList.map((book) => (
+                                <li key={book.id}>
+                                    <BookItem
+                                        book={book}
+                                    />
+                                </li>
+                            ))}
+                        </ol>
+                    </div>
+                )
+            } else {
+                return (<div></div>)
+            }
+
+        } else {
+            bookList = books.filter((book) => book.shelf === this.props.value)
+
             return (
                 <div className="bookshelf">
                     <h2 className="bookshelf-title">{this.props.title}</h2>
